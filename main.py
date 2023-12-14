333b0d386ec676902b7560e14dcfab2efc0f49b3
GrigaKarbon <151057180+GrigaKarbon@users.noreply.github.com>
2023-12-14 21:54:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,52 +8,52 @@ fps = 50  # количество кадров в секунду
 clock = pygame.time.Clock()
 running = True
 
-def generate_level(x, y):
-    con = sqlite3.connect("rooms_bd.sqlite")
-    cur = con.cursor()
-    with open('generated_map.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        row_dull = '000'
-        row_wall = '002'
-        row_list = []
-        for i in range(y + 2):
-            listik = []
-            if i == 0 or i == y + 2:
-                for j in range(x + 2):
-                    lisik.append('002')
-                row_list.append(listik)
-            else:
-                for j in range(x + 2):
-                    if j == 0 or j == y + 2:
-                        lisik.append('002')
-                    else:
-                        lisik.append('000')
-                    listik.append(row_list)
-
-        writer.writerow(row_list)
-    c = cur.execute("""SELECT * FROM rooms WHERE type=?""", ('ess_room',)).fetchall()
-    used_tiles = set()
-    for room in c:
-        rx = random.randint(0, x)
-        ry = random.randint(0, y)
-        if rx not in used_tiles and ry not in used_tiles:
-            used_tiles.add(rx)
-            used_tiles.add(ry)
-            # тут будет добавление комнаты на карту, пока делаю(
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [['000'] * width for _ in range(height)]
+        used_coor = []
+        for i in range(self.width):
+            for j in range(self.width):
+                used_coor.append((j, i))  # пустая карта, карта всегда квадрат, для удобства
 
 
-        # тут будут генерации по созданной пустой карте
+        con = sqlite3.connect("rooms_bd.sqlite")
+        cur = con.cursor()
+        c = cur.execute("""SELECT * FROM rooms WHERE type=?""", ('ess_room',)).fetchall()  # осн генерация
+        for room in c:
+            rcoor = random.choice(used_coor)
+            used_coor.remove(rcoor)
+            self.board[rcoor[0]][rcoor[1]] = room[2]
 
-while running:  # главный игровой цикл
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if self.width >= 5 and self.height >= 5:
+            c = cur.execute("""SELECT * FROM rooms WHERE type=?""", ('room',)).fetchall()  # генерация доп комнат
+            for i in range(self.width - 3):
+                rroom = random.randint(0, len(c) - 1)
+                rcoor = random.choice(used_coor)
+                used_coor.remove(rcoor)
+                self.board[rcoor[0]][rcoor[1]] = c[rroom][2]
+            c = cur.execute("""SELECT * FROM rooms WHERE type=?""", ('monstr',)).fetchall()  # генерация монстров (пока только 1((()
+            for i in range(self.width - 3):
+                rmon = random.randint(0, len(c) - 1)
+                rcoor = random.choice(used_coor)
+                used_coor.remove(rcoor)
+                self.board[rcoor[0]][rcoor[1]] = c[rmon][2]
+
+        print(*self.board, sep='\n')
+
+# while running:  # главный игровой цикл
+    # for event in pygame.event.get():
+        # if event.type == pygame.QUIT:
+            # running = False
         # обработка остальных событий
     # ...
     # формирование кадра
     # ...
-    pygame.display.flip()  # смена кадра
+    # pygame.display.flip()  # смена кадра
     # изменение игрового мира
     # ...
     # временная задержка
-    clock.tick(fps) # тут впринципе для федора все
+    # clock.tick(fps) # тут впринципе для федора все
+cc = Board(10, 10)  # оно в принте отображает созданный лабиринт, цифры - айди комнаты в бд
